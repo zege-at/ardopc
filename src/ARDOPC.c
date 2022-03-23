fb1e375053f3498c59d2e4a0b9910f28a9f85414
Gerald Zehetner <gerald@oe5gze.radio>
2022-03-23 10:05:06 +0100

    Added Copyright

--- src/ARDOPC.c
+++ src/ARDOPC.c
@@ -1,11 +1,30 @@
-// ARDOPC.cpp : Defines the entry point for the console application.
-//
+/* ARDOPC.c
+ *
+ * Copyright 2001 John Wiseman G8BPQ
+ * Copyright 2022 Gerald Zehetner OE5GZE
+ *
+ *
+ * This file is part of ARDOPC.
+ *
+ * ARDOPC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARDOPC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+// ARDOPC.c : Defines the entry point for the console application.
 
 #ifdef WIN32
 #define _CRT_SECURE_NO_DEPRECATE
 #define _USE_32BIT_TIME_T
-
 #include <windows.h>
 #include <mmsystem.h>
 #pragma comment(lib, "winmm.lib")
@@ -15,8 +34,10 @@
 #define closesocket close
 #endif
 
+#include <getopt.h>
+
 #include "ARDOPC.h"
-#include "getopt.h"
+
 
 void CompressCallsign(char * Callsign, UCHAR * Compressed);
 void CompressGridSquare(char * Square, UCHAR * Compressed);
